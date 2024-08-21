30c695757a51d3684c0ad59f6a3ef30af67cefc7
zf17 <z.f17@outlook.com>
2024-08-21 15:58:37 +0300

    updating code

--- Week2/plates.py
+++ Week2/plates.py
@@ -1,9 +1,13 @@
-def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
+# “All vanity plates must start with at least two letters.”
+# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+# “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate;
+# AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+# “No periods, spaces, or punctuation marks are allowed.”
+
+# In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or 
+# Invalid if it does not. Assume that any letters in the user’s input will be uppercase. Structure your program per the below, wherein 
+# is_valid returns True if s meets all requirements and False if it does not. Assume that s will be a str. You’re welcome to implement 
+# additional functions for is_valid to call (e.g., one function per requirement).
 
 
 def is_valid(s):
@@ -19,15 +23,14 @@ def is_valid(s):
         if s[i] == "0" and s[i-1].isnumeric() == False:
             return False
     return True
-main()
 
-# “All vanity plates must start with at least two letters.”
-# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
-# “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate;
-# AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
-# “No periods, spaces, or punctuation marks are allowed.”
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
-# In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or 
-# Invalid if it does not. Assume that any letters in the user’s input will be uppercase. Structure your program per the below, wherein 
-# is_valid returns True if s meets all requirements and False if it does not. Assume that s will be a str. You’re welcome to implement 
-# additional functions for is_valid to call (e.g., one function per requirement).
+if __name__ == "__main__":
+    main()
+
--- Week2/twttr.py
+++ Week2/twttr.py
@@ -1,3 +1,8 @@
+# When texting or tweeting, it’s not uncommon to shorten words to save time or space, as by omitting
+# vowels, much like Twitter was originally called twttr. In a file called twttr.py, implement a program 
+# that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O,
+# and U) omitted, whether inputted in uppercase or lowercase.
+
 text = input("Input: ")
 vowels = ["a", "e", "i", "o", "u"]
 new_text = ""
@@ -6,7 +11,3 @@ for letter in text:
         new_text += letter
 print(f"Output: {new_text}")
 
-# When texting or tweeting, it’s not uncommon to shorten words to save time or space, as by omitting
-# vowels, much like Twitter was originally called twttr. In a file called twttr.py, implement a program 
-# that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O,
-# and U) omitted, whether inputted in uppercase or lowercase.
